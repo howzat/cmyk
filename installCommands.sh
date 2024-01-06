4c3590c6261901e01094862dd939d298b597f903
Ben Letton <benletton@gmail.com>
2024-01-06 13:04:27 +0000

    [fix] Add deployer role

--- installCommands.sh
+++ installCommands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
 echo "INSTALL COMMANDS"
 npm ci
 npm run exportEnv
--- installSteps.sh
+++ installSteps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
+set -x
 echo "INSTALL STEPS"
 npm run sls deploy --stage=$STAGE
