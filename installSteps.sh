0931dfb5a2e75e337b40b87908a3246719ff4201
Ben Letton <benletton@gmail.com>
2024-01-10 15:56:35 +0000

    [fix] Improve the install steps

--- installSteps.sh
+++ installSteps.sh
@@ -5,10 +5,10 @@ export AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN
 export SLS_STAGE
 
-echo "INSTALL STEPS ${DEV_ACCOUNT_ID} - ${SLS_STAGE}"
+echo "INSTALL STEPS ${DEV_ACCOUNT_ID} - ${SLS_STAGE} - ${AWS_REGION}"
 CREDS=$(aws sts assume-role --role-arn arn:aws:iam::"${DEV_ACCOUNT_ID}":role/CMYKDeployerRole --role-session-name=CMYKDeployerRole)
 AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
 AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
 AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
 npm run exportEnv
-npm run sls deploy --stage=${SLS_STAGE}
+npm run sls deploy --stage="${SLS_STAGE}" --verbose --region="${AWS_REGION}"
